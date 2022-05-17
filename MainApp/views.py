d7b28751c878b9696da67aaba98b3f236c5bb0cb
boo <booblegum42@gmail.com>
2022-05-17 10:43:05 -0700

    task1,2

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render, HttpResponse
 
 name = "Евгений"
 surname = "Юрченко"
+email = "eyurchenko@specialist.ru"
 
 
 def about(request):
-    info = f"<h1>Автор: <i>{name} {surname}</i></h1>"
+    info = f""" <div>Имя: <b>{name}</b> </div>
+                Фамилия: <b>{surname}</b> <br>
+                телефон: <b>8-923-600-01-02</b> <br>
+                email: <b>{email}</b>"""
     return HttpResponse(info)
 
 
 def home(request):
-    return HttpResponse("Hello")
+    text = f"""<h1> "Изучаем django" </h1>
+    <strong> Автор </strong>: <i> {surname} Е.В. </i>"""
+    return HttpResponse(text)
