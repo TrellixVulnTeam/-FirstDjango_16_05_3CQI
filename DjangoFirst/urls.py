9f9585ee6578ae0e94bf65560c8fa502d3b036da
boo <booblegum42@gmail.com>
2022-05-17 11:09:33 -0700

    task 3,4

--- DjangoFirst/urls.py
+++ DjangoFirst/urls.py
@@ -5,4 +5,5 @@ from MainApp import views
 urlpatterns = [
     path('', views.home),  # http://127.0.0.1:8000
     path('about', views.about),  # http://127.0.0.1:8000/about
+    path('item/<int:id>', views.item_page) # http://127.0.0.1:8000/item/1 http://127.0.0.1:8000/item/2
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render, HttpResponse
 name = "Евгений"
 surname = "Юрченко"
 email = "eyurchenko@specialist.ru"
+items = [
+    {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
+    {"id": 2, "name": "Куртка кожаная", "quantity": 2},
+    {"id": 3, "name": "Coca-cola 1 литр", "quantity": 12},
+    {"id": 4, "name": "Картофель фри", "quantity": 0},
+    {"id": 5, "name": "Кепка", "quantity": 124},
+]
 
 
 def about(request):
@@ -17,3 +24,12 @@ def home(request):
     text = f"""<h1> "Изучаем django" </h1>
     <strong> Автор </strong>: <i> {surname} Е.В. </i>"""
     return HttpResponse(text)
+
+
+def item_page(request, id):
+    for item in items:
+        if item["id"] == id:
+            item_str = f"Товар: <b>{item['name']}</b> количество: {item['quantity']}"
+            return HttpResponse(item_str)
+
+    return HttpResponse(f"Товар c id={id} не найден")
