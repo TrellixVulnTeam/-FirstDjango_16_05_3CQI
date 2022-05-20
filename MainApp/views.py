61a85bb74c73f52ecc185d8d902adaed6f26f88e
boo <booblegum42@gmail.com>
2022-05-20 08:57:58 -0700

    add db Items

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,27 +1,6 @@
 from django.shortcuts import render, HttpResponse, Http404
-
-name = "Евгений"
-surname = "Юрченко"
-email = "eyurchenko@specialist.ru"
-phone = "8-923-600-01-02"
-
-items = [
-    {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
-    {"id": 2, "name": "Куртка кожаная", "quantity": 2},
-    {"id": 3, "name": "Coca-cola 1 литр", "quantity": 12},
-    {"id": 4, "name": "Картофель фри", "quantity": 0},
-    {"id": 5, "name": "Кепка", "quantity": 124},
-]
-
-
-def about(request):
-    context = {
-        "name": "Евгений",
-        "surname": "Юрченко",
-        "phone": 79008001012,
-        "email": [2, 5, "6", 7.6, 8]
-    }
-    return render(request, "about.html", context)
+from MainApp.models import Item
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def home(request):
@@ -29,14 +8,15 @@ def home(request):
 
 
 def item_page(request, id):
-    for item in items:
-        if item["id"] == id:
-            return render(request, "item_page.html", item)
-
-    raise Http404(f"Товар c id={id} не найден")
+    try:
+        item = Item.objects.get(pk=id)
+        return render(request, "item_page.html", {"item": item})
+    except ObjectDoesNotExist:
+        raise Http404(f"Товар c id={id} не найден")
 
 
 def items_list(request):
+    items = Item.objects.all()
     context = {
         "items": items
     }
